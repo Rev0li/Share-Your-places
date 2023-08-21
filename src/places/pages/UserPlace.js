00c2939a094b51fa1291bffeea5d01bae2cdd209
Revoli <kientzler.oliver@gmail.com>
2023-08-21 13:10:07 -0400

    add modal google map

--- src/places/pages/UserPlace.js
+++ src/places/pages/UserPlace.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -31,5 +33,7 @@ const DUMMY_PLACES = [
 ];
 
 export default function UserPlace() {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 }
