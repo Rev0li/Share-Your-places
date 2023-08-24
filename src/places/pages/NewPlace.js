2c8837b6c4dabfc717f155d7265cbc6a45157e30
Revoli <kientzler.oliver@gmail.com>
2023-08-24 09:12:06 -0400

    input validator isValid logic

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useCallback } from "react";
+
 import "./NewPlace.css";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
-  return <Input element="input" type="text" label="Title" />;
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter valid Descritpion (min 5 length)"
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
