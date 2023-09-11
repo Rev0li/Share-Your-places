4779d03224c4e6eacf26d096bf187ac37f8a0f56
Revoli <kientzler.oliver@gmail.com>
2023-09-11 17:45:05 -0400

    express validator

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
+const { validationResult } = require('express-validator');
+
 const HttpError = require('../models/http-error');
 
 let DUMMY_PLACES = [
@@ -41,6 +43,11 @@ const getPlacesByUserId = (req, res, next) => {
 }
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+  }
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
     id: uuidv4() ,
