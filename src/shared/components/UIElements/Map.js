0f9cd943b943f0e0ed6dea1f56d0a22dee77cfa5
Revoli <kientzler.oliver@gmail.com>
2023-09-13 15:38:27 -0400

    try resolve security api key in frontend

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,22 +1,42 @@
 import React, { useRef, useEffect } from "react";
 import "./Map.css";
 
+import { Loader } from "@googlemaps/js-api-loader";
+require("dotenv").config();
+
 const Map = (props) => {
+  const MAP_API_KEY = process.env.MY_KEY;
+
+  const loader = new Loader({
+    apiKey: MAP_API_KEY,
+    version: "weekly",
+  });
+
   const mapRef = useRef();
 
   const { center, zoom } = props;
 
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom,
+    loader.load().then(async () => {
+      const { Map } = await window.google.maps.importLibrary("maps");
+
+      const map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+      new window.google.maps.Marker({ position: center, map: map });
     });
 
-    new window.google.maps.Marker({ position: center, map: map });
+    // const map = new window.google.maps.Map(mapRef.current, {
+    //   center: center,
+    //   zoom: zoom,
+    // });
+    // new window.google.maps.Marker({ position: center, map: map });
   }, [center, zoom]);
 
   return (
     <div
+      id="map"
       ref={mapRef}
       className={`map ${props.className}`}
       style={props.style}
---
+++ src/shared/util/map.js
@@ -0,0 +1,19 @@
+import { Loader } from "@googlemaps/js-api-loader";
+require("dotenv").config();
+
+const MAP_API_KEY = process.env.MY_KEY;
+
+const loader = new Loader({
+  apiKey: MAP_API_KEY,
+  version: "weekly",
+  ...additionalOptions,
+});
+
+loader.load().then(async () => {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
